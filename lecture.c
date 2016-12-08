147c8aefd6e542681ff056772d7421a39d322957
pierre-geeraert <pierre.geeraert@viacesi.fr>
2016-12-08 17:19:31 +0100

    code qui permet de lire un txt (ou pbm)
    code qui permet de lire un txt (ou pbm) et reconnaitre si c est un 0 ; le but est de mettre " " quand il detecte 0 et de metre "*" quand il detecte 1 et mettre a la laigne quand le caractere suivant est plusieurs  espaces

---
+++ lecture.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+
+{
+
+    FILE* fichier = NULL;
+
+    int caractereActuel = 0;
+    int compt = 0;
+
+ 
+
+    fichier = fopen("image2", "r");
+
+ 
+
+    if (fichier != NULL)
+
+    {
+
+        // Boucle de lecture des caractères un à un
+
+        do
+
+        {
+
+            caractereActuel = fgetc(fichier); // On lit le caractère
+
+            //printf("%c", caractereActuel); // On l'affiche
+	if (caractereActuel == 48)
+{
+printf("*");
+compt++;
+}
+
+        } while (caractereActuel != EOF); // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
+
+ 
+
+        fclose(fichier);
+
+    }
+
+ 
+
+    return 0;
+
+}
