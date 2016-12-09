96e94a347fa440101e8cbf851091ecf4a61d1c5c
pierre-geeraert <pierre.geeraert@viacesi.fr>
2016-12-09 15:18:44 +0100

    upgrade code to add alea in this code
    add indentation too

--- lecture.c
+++ lecture.c
@@ -1,58 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>//utile pour la creation d'un nombre aleatoire
 
 int main(int argc, char *argv[])
 
 {
+system("clear");
+    file* fichier = null;
 
-    FILE* fichier = NULL;
+    int caractereactuel = 0;
+    int nbrAlea;
+    int linecounter = 0;
 
-    int caractereActuel = 0;
-    
-    int lineCounter = 0;
+ 
+	srand (time (NULL));//créer nbr alea entre 1 et 5 pour choisir entre les differentes images
+	nbrAlea = Random (1,5);
+	printf ("Random : %d", nbrAlea);
+	fichier = fopen("image2", "r");
 
  
 
-    fichier = fopen("image1", "r");
-
- 
-
-    if (fichier != NULL)
+    if (fichier != null)
 
     {
 
-        // Boucle de lecture des caractres un  un
+        // boucle de lecture des caractres un  un
 
         do
 		
 
         {
 
-            caractereActuel = fgetc(fichier); // On lit le caractre
+            caractereactuel = fgetc(fichier); // on lit le caractre
 
-          
+           // printf("%c", caractereactuel); // on l'affiche
+	
+	//printf("%d",compt); 
 
-	switch(caractereActuel)
-{
-case 49: //1
-	printf("%c",254);
-break;
-case 48: //0
-	printf("%c",240);
-break;
-case '\t': //tab
-	//printf("er");
-break;
-default:
-	printf("\n");
-	lineCounter++;
-break;
-}
+		switch(caractereactuel)
+	{
+	case 49: //1
+		printf("%c",987);
+
+	break;
+	case 48: //0
+		printf("%c",240);
+	break;
+	case '\t': //tab
+		//printf("er");
+	break;
+	//case 
+	default:
+		printf("\n");
+		linecounter++;
+	break;
+	}
 
 	
 
 
-        } while (lineCounter < 24 ); // On continue tant que compteurglobal n'a pas finis le fichier
+        } while (linecounter < 30 ); // on continue tant que compteurglobal n'a pas finis le fichier
 
  
 
@@ -61,7 +68,13 @@ break;
     }
 
  
+while(1);//attente pour eviter de montrer le prompt 
 
     return 0;
 
 }
+	/*fonction qui permet de creer le random en fonction des resultats precedents*/
+	int random (int _imin, int _imax)
+	{
+	return (_imin + (rand () % (_imax-_imin+1)));
+	}
