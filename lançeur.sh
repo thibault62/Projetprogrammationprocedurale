eb2a5ab4e2c6c6b98977a25e79eccd7e23c1dd5a
pierre-geeraert <pierre.geeraert@viacesi.fr>
2016-12-11 21:13:08 +0100

    upgrade lanceur.sh and prepare to add time

--- lançeur.sh
+++ lançeur.sh
@@ -1,14 +1,40 @@
 #ce programme permet de choisir entre les 3 ecrans de verrouillage
 min=1
-max=3
+max=1
 number=$[($RANDOM % ($[$max - $min] + 1)) + $min]
 #changement de repertoire pour utiliser chemins relatif(plus court et moins d erreur)
 cd /media/groupe-tvp/b919f1b0-9122-4865-a76b-bb6386984916/doc
 
-#case qui permet de choisir entre les ecrans de verouilage sne changenat de repertoire , en compilant pour avoir un executable le plus recent possible puis en l'executant
+#IF qui permet de choisir entre les ecrans de verouilages en changeant de repertoire , en compilant pour avoir un executable le plus recent possible puis en l'executant
 
-case $number in
-   1) cd statique;gcc -o lecture2 lecture2.c;./lecture2;;
-   2) cd dynamique;gcc -o lecture2 lecture2.c;./lecture2;;
-   3) cd interactif;gcc -o lecture2 lecture2.c;./lecture2;;
-esac
+#utilisation de IF a la place de CASE car beaucoup d instruction a ecrire et donc plus lisible avec des IF
+
+
+	if [ $number == 1 ]
+	then
+	gcc -o heure-vrai heure-vrai.c
+	./heure-vrai
+	cd statique;
+	
+	gcc -o lecture2 lecture2.c;
+	./lecture2
+	
+	fi
+
+
+	if [ $number == 2 ]
+	then
+	cd dynamique;
+	rm lecture2;
+	gcc -o lecture2 lecture2.c;
+	./lecture2
+	fi
+
+
+	if [ $number == 3 ]
+	then
+	cd interactif;
+	rm lecture2;
+	gcc -o lecture2 lecture2.c;
+	./lecture2
+	fi
