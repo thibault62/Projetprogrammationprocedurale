dab90b76fb04c5efbbfd81117ed0748a78ff3e23
pierre-geeraert <pierre.geeraert@viacesi.fr>
2016-12-11 22:14:26 +0100

    now this programm is stopped by any key

--- lecture.c
+++ lecture.c
@@ -1,12 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>//utile pour la creation d'un nombre aleatoire
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 
 /*fonction qui permet de creer le random en fonction des resultats precedents*/
 	int Random (int _imin, int _imax)
 	{
 	return (_imin + (rand () % (_imax-_imin+1)));
 	}
+
+/*touche*/
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+ 
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+ 
+  ch = getchar();
+ 
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+ 
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+ 
+  return 0;
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main(int argc, char *argv[])
 
@@ -16,7 +62,7 @@ int main(int argc, char *argv[])
    	FILE* fichier = NULL;
    	FILE* history = NULL;
 	history = fopen("/media/groupe-tvp/b919f1b0-9122-4865-a76b-bb6386984916/doc/history.txt", "a");
-	fprintf(history," statique\n");
+	fprintf(history," statique ");
 
 
 
@@ -29,23 +75,28 @@ int main(int argc, char *argv[])
 	nbrAlea = Random (1,5);
 	
 
-	
+	fprintf(history," image: ");
 /*switch qui permet d'ouvrir une image en fonction d'un nombre aleatoire entre 1 et 5 */
 	switch(nbrAlea)
 {case 1:
 fichier = fopen("image1", "r");
+fprintf(history," je suis en pause");
 break;
 case 2:
 fichier = fopen("image2", "r");
+fprintf(history," tetris");
 break;
 case 3:
 fichier = fopen("image3", "r");
+fprintf(history," start to continue");
 break; 
 case 4:
 fichier = fopen("image4", "r");
+fprintf(history," psychédélique");
 break;
 case 5:
 fichier = fopen("image5", "r");
+fprintf(history," jul");
 break;
 default:
 break;
@@ -100,11 +151,12 @@ break;
     }
 
  
-//while(1);//attente pour eviter de montrer le prompt 
+while(!kbhit())
+ printf("");
 
     return 0;
 
 }
-	
+
 
    
