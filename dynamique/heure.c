2ff4c53970df1aa9139eb4138c5174e31acd2b4c
pierre-geeraert <pierre.geeraert@viacesi.fr>
2016-12-14 09:34:44 +0100

    update code heure.c

--- dynamique/heure.c
+++ dynamique/heure.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include </media/groupe-tvp/b919f1b0-9122-4865-a76b-bb6386984916/doc/dynamique/exia.h>//permet la fonction ouverture
 
 FILE* ouverture(int temps);
 
@@ -53,43 +54,4 @@ while(1)
     return 0;
 
 }
-/*fonction qui permet l'ouverture des fichiers*/
-FILE* ouverture(int temps) {//----ouverture
-FILE* fichier=NULL;
-switch(temps)
-{
-case 0:
-fichier = fopen("0","r");
-break;
-case 1:
-fichier = fopen("1","r");
-break;
-case 2:
-fichier = fopen("2","r");
-break;
-case 3:
-fichier = fopen("3","r");
-break;
-case 4:
-fichier = fopen("4","r");
-break;
-case 5:
-fichier = fopen("5","r");
-break;
-case 6:
-fichier = fopen("6","r");
-break;
-case 7:
-fichier = fopen("7","r");
-break;
-case 8:
-fichier = fopen("8","r");
-break;
-case 9:
-fichier = fopen("9","r");
-break;
-default:
-break;
-return fichier;
-}
-}//------------------------ouverture
+
